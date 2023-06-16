8a8d481abf258058900f562c69a7b274b8e58af2
ogurayusuke <oguyu2021@gmail.com>
2023-06-16 17:49:48 +0900

    ワーク提出

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,59 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = current_user.blogs.build(blog_params)
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+      render :new
+      end
+    end
+  end
+  
+  def confirm
+    @blog = current_user.blogs.build(blog_params)
+    render :new if @blog.invalid?
+  end
+
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+  end
+
+  def edit
+  end
+
+  def updated
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログ削除しました！"
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+end
